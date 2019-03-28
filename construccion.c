c2f30251bbdb24525ce03033c49435c7ecc2f512
Federico <matiasiman@gmail.com>
2019-03-27 21:57:56 -0300

    Orden de la matriz

--- construccion.c
+++ construccion.c
@@ -1,33 +1,41 @@
 #include  "grafo.c"
 
+int cmp(const void* a, const void* b) {
+    u32 *ap = *(u32**)a, *bp = *(u32**)b;
+    u32 fsta = *ap, fstb = *bp;
+    u32 snda = *(++ap), sndb = *(++bp);
+    if(fsta == fstb) return snda - sndb;
+    else return fsta - fstb;
+}
 int main(){
-	Grafo g = malloc(sizeof(struct GrafoSt)); 				//Declaro variable de tipo Grafo
+	Grafo g = (Grafo)malloc(sizeof(struct GrafoSt)); 		//Declaro variable de tipo Grafo
 	printf("numero de vertices:"); 			  				//Pido y 
-	scanf("%u",&g->n);			  			  				//guardo n° de vertices
+	if(scanf("%u",&g->n) < 0) return 1;			  			//guardo n° de vertices
 	printf("numero de aristas:"); 			  				//Pido y 
-	scanf("%u",&g->m);           			  				//guardo n° de aristas
-	g->vertices = malloc(2*sizeof(u32*));     				//g->vertices = array de 2 punteros de u32
-	for(u32 i = 0; i < 2 ; i++){							//Para cada puntero 
-        g->vertices[i] = malloc((2*g->m)*sizeof(u32));		//le asigno un array de 2*g->m de u32
+	if(scanf("%u",&g->m) < 0) return 1;           			//guardo n° de aristas
+	g->vertices = (u32**)malloc((2*(g->m))*sizeof(u32*));   //g->vertices = array de 2 punteros de u32
+	for(u32 i = 0; i < 2*(g->m) ; i++){						//Para cada puntero 
+        g->vertices[i] = (u32*)malloc(2*sizeof(u32));		//le asigno un array de 2*g->m de u32
 	}														
-	for(u32 i = 0;i< g->m;i++){ 							//Para cada columna de la matriz 
+	for(u32 i = 0; i < g->m; i++){ 							//Para cada columna de la matriz 
 		printf("conexiones:");								//completo las aristas
-		scanf("%u",&g->vertices[0][i]);						//y las guardo en sus correspondientes lugares
-		scanf("%u",&g->vertices[1][i]);
+		if (scanf("%u", &g->vertices[i][0]) < 0) return 1 ;	//y las guardo en sus correspondientes lugares
+		if (scanf("%u", &g->vertices[i][1]) < 0) return 1;
 	}
 	int j = 0;												//Declro un variable para iterar en columnas
-	for(u32 i = g->m;i<(2*g->m);i++){						//For para recorrer desde g->m hasta 2*(g->m)
-		g->vertices[0][i] = g->vertices[1][j];				//y completar con sus lugares pero invertidos
-		g->vertices[1][i] = g->vertices[0][j];
+	for(u32 i = g->m; i<(2*g->m); i++){				    	//For para recorrer desde g->m hasta 2*(g->m)
+		g->vertices[i][0] = g->vertices[j][1];				//y completar con sus lugares pero invertidos
+		g->vertices[i][1] = g->vertices[j][0];
 		j++;
 	}
-	for(int i = 0; i<2;i++){
-		for(u32 j = 0;j<(2*g->m);j++){
-			printf("%u",g->vertices[i][j]);
+	qsort(g->vertices, 2*(g->m), 8, cmp);
+	for(u32 i = 0; i< 2*g->m; i++){
+		for(int j = 0;j<2;j++){
+			printf("%u ", g->vertices[i][j]);
 		}
 		printf("\n");
 	}
-	for(int i = 0;i<2;i++){									//Recorro el arreglo de punteros principales
+	for(u32 i = 0;i<(2*g->m);i++){							//Recorro el arreglo de punteros principales
 		free(g->vertices[i]);								//y los libero
 	}												
 	free(g->vertices);										//Libero el puntero al arreglo de punteros 

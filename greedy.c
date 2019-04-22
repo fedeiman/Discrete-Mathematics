31025a2e9f323f0f4809927968fdb33b909a71f4
Federico <matiasiman@gmail.com>
2019-04-22 00:28:30 -0300

    Greedy funcionando

---
+++ greedy.c
@@ -0,0 +1,73 @@
+#ifndef _GREEDY
+#define _GREEDY
+
+#include "Rii.h"
+#include "grafo.c"
+#include "orden.c"
+#include "vecinos.c"
+
+u32 binaria(Orden *o, u32 dato, u32 array_lenght){
+  u32 inf = 0; 
+  u32 sup = array_lenght - 1;
+  u32 mitad;
+  while(inf<=sup){
+    mitad = (inf + sup)/2;
+    if(o[mitad]->nombre == dato){
+      return mitad;
+    }
+    if(o[mitad]->nombre > dato){
+      sup = mitad - 1;
+    }
+    if(o[mitad]->nombre < dato){
+      inf = mitad + 1;
+    }
+  }
+  //Esta linea se ejecutara solo en caso de que el "dato"
+  //a buscar no este en v[n]->nombre_del_vetice.
+  //Cosa que no puede pasar.
+  printf("ERROR");
+  return(-1);
+}
+
+u32 max(u32 i, u32 j){
+  if(i <= j) return j;
+  else return i;
+}
+
+u32 Greedy(Grafo G){
+  u32 *sinusar = (u32*)calloc((G->n),sizeof(u32));
+  u32 *usados = (u32*)calloc((G->n),sizeof(u32));
+  u32 c = 0;
+  u32 color = 0;
+  u32 resultado = 0;
+  
+  for(u32 i = 0; i < G->n ; i++){
+    G->o[i]->color_actaul = ~0u;
+  }
+  for(u32 i = 0; i < G->n; i++){
+    for(u32 j = 0; j < (G->v[i]->ind_de_final_vecinos - G->v[i]->ind_de_inicio_vecinos + 1); j++){
+      if(G->o[G->v[i]->array_vecinos[j]]->color_actaul != ~0u){
+        sinusar[G->o[G->v[i]->array_vecinos[j]]->color_actaul] = 1;
+        usados[c++] = G->o[G->v[i]->array_vecinos[j]]->color_actaul;
+      }
+    }  
+    while(sinusar[color] == 1){
+        color++;
+    }
+    resultado = max(resultado,color + 1);
+    G->v[i]->color = color;
+    G->o[binaria(G->o,G->v[i]->nombre_del_vertice,G->n)]->color_actaul = color;
+    color = 0;
+    while(c){ 
+      sinusar[usados[--c]] = 0;
+    }
+    //memset(sinusar,0,G->n*sizeof(u32));
+  }
+
+  if(sinusar) free(sinusar);
+  if(usados) free(usados);
+  
+  return resultado;
+}
+
+#endif
--- main.c
+++ main.c
@@ -5,54 +5,14 @@
 #include "grafo.c"
 #include "vecinos.c"
 #include "orden.c"
+//#include "greedy.c"
 int main(){
 	Grafo g;
-
-	Grafo h;
+	u32 j;
 	g = ConstruccionDelGrafo();
-	//int n;
-	h = CopiarGrafo(g);
-	for(u32 i = 0; i < g->n; i++){
-		printf("%u",g->v[i]->total_vecinos);		
-	}
-	printf("\n");
-	for(u32 i = 0; i < g->n; i++){
-		printf("%u",h->v[i]->total_vecinos);
-	}
-	for(u32 i = 0; i < g->n; i++){
-		printf("%u",g->v[i]->nombre_del_vertice);
-	}
-	printf("\n");
 	OrdenWelshPowell(g);
-	for(u32 i = 0; i < g->n; i++){
-		printf("%u",g->v[i]->nombre_del_vertice);
-	}
-	printf("\n");
-	printf("%u\n",g->v[0]->total_vecinos);
-	printf("%u\n",g->v[1]->total_vecinos);
-	SwitchVertices(g,0,1);
-	for(u32 i = 0; i < g->n; i++){
-		printf("%u",g->v[i]->nombre_del_vertice);
-	}
-	printf("\n");
-	//u32 n = NombreJotaesimoVecino(g,1,1);
-	//u32 j = GradoDelVertice(g,3);
-	//printf("%u\n",j);
-	//printf("%u\n",n);
-	printf("%u\n",g->v[0]->total_vecinos);
-	printf("%u\n",g->v[1]->total_vecinos);
-	g->colores = 200;
-	g->o[0]->color_actaul = 10;
-	g->o[1]->color_actaul = 56;
-	g->o[2]->color_actaul = 56;
-	printf("color init %u\n",g->o[1]->color_actaul);
-	printf("color init %u\n",g->o[0]->color_actaul);
-	printf("color init %u\n",g->o[2]->color_actaul);
-	SwitchColores(g,10,56);
-	printf("color switch %u\n",g->o[1]->color_actaul);
-	printf("color switch %u\n",g->o[0]->color_actaul);
-	printf("color init %u\n",g->o[2]->color_actaul);
-	DestruccionDelGrafo(h);
+	j = Greedy(g);
+	printf("%u\n",j);
 	DestruccionDelGrafo(g);
 
 }
